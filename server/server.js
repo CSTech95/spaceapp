a8a53116e07e7647ee7df4231e64de130c961b8e
Aser <techdr95@gmail.com>
2020-08-16 23:40:20 -0400

    added API Key to server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 
 const app = express();
 
+const api_key = process.env.API_KEY;
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
